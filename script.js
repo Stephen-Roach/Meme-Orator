9aeaf0a806d55e3534db6577d02d22afa9def945
JohnLoftin <jgloftin@gmail.com>
2020-09-02 16:04:21 -0400

    added fetch functionality to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(() => {
+    // $(".search-bar").click(function(e){
+    //     e.preventDefault();
+    //     //let searchString = $(".search-bar").val();
+    //     //let urlEncodedSearchString = encodeURIComponent(searchString);
+    //     fetch("https://api.imgflip.com/get_memes")
+    //         .then(response => response.json())
+    //         .then(response => {
+    //             //renderMemes(response.Search);
+    //             console.log(response);
+    //         })
+    // });
+    fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                //renderMemes(response.Search);
+                console.log(response);
+            })
+});
