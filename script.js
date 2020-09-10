a4e31e7c88fc04b60eb1fd77f1cfe3a5280dfcd1
JohnLoftin <jgloftin@gmail.com>
2020-09-10 14:04:33 -0400

    added search functionality to searchbar and moved both api calls to click function

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 $(() => {
 
-    $(".blah").click(function(e){
+    document.cookie = "SameSite=None; Secure"
+
+    $("#search-size").click(function(e){
         e.preventDefault();
-        let searchString = $(".blah").val();
-        // let urlEncodedSearchString = encodeURIComponent(searchString);
+        let searchString = $("#search-size").val();
         fetch(`http://api.giphy.com/v1/gifs/search?q=${searchString}&api_key=RJpUMPEri2bZmuX4lkjaIX9NeuYWQjfA&`)
             .then(response => response.json())
             .then(response => {
-                //renderMemes(response.data.embed_url);
-                console.log(response.data);
+                renderGifs(response.data);
+                //console.log(response.data);
             });
-    });
-
-    fetch("https://api.imgflip.com/get_memes")
+        fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
             .then(response => {
                 renderMemes(response.data.memes);
+                //console.log(response.data.memes);
             });
+    });
             
     function renderMemes(memeArray){
+        let searchString = $("#search-size").val();
+        searchString.toLowerCase();
         memeArray.map((memeItem) => {
+            if(memeItem.name.toLowerCase() === searchString){
+                $(".meme-container").append(`
+                    <div class="meme">
+                    <div class="card" style="width: 18rem;">
+                    <img id="meme-image" class="card-img-top" src="${memeItem.url}" alt="Card image cap">
+                    </div>
+                    </div>
+                `);
+            }
+        });
+    }
+    function renderGifs(gifArray){
+        gifArray.map((gifItem) => {
             $(".meme-container").append(`
                 <div class="meme">
                 <div class="card" style="width: 18rem;">
-                <img id="meme-image" class="card-img-top" src="${memeItem.url}" alt="Card image cap">
+                <img id="meme-image" class="card-img-top" src="${gifItem.embed_url}" alt="Card image cap">
                 </div>
                 </div>
-            `);
-        });
+            `)
+        })
     }
-    $( "#date" ).datepicker();
 });
