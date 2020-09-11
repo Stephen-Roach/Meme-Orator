fd69c411196fa66a4851abe4c12eac91b76d3725
Heather Luttrell <possumdiva@heatherluttrell.com>
2020-09-10 20:43:47 -0400

    input buttons glow

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
 $(() => {
+  $("#search-btn").click(function (e) {
+    e.preventDefault();
+    let searchString = $("#search-size").val();
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        renderMemes(response.data.memes);
+      });
+    fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${searchString}&api_key=RJpUMPEri2bZmuX4lkjaIX9NeuYWQjfA&`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        renderGifs(response.data);
+      });
+  });
 
-    $("#search-btn").click(function(e){
-        e.preventDefault();
-        let searchString = $("#search-size").val();
-        fetch("https://api.imgflip.com/get_memes")
-            .then(response => response.json())
-            .then(response => {
-                renderMemes(response.data.memes);
-            });
-        fetch(`http://api.giphy.com/v1/gifs/search?q=${searchString}&api_key=RJpUMPEri2bZmuX4lkjaIX9NeuYWQjfA&`)
-            .then(response => response.json())
-            .then(response => {
-                renderGifs(response.data);
-            });
-    });
-            
-    function renderMemes(memeArray){
-        let searchString = $("#search-size").val();
-        searchString.toLowerCase();
-        memeArray.map((memeItem) => {
-            if(memeItem.name.toLowerCase().includes(searchString)){
-                $(".meme-container").append(`
+  function renderMemes(memeArray) {
+    let searchString = $("#search-size").val();
+    searchString.toLowerCase();
+    memeArray.map((memeItem) => {
+      if (memeItem.name.toLowerCase().includes(searchString)) {
+        $(".meme-container").append(`
                     <div class="meme">
                     <div class="card" style="width: 18rem;">
                     <a href="#" onclick="selectMeme('${memeItem.url}')"><img id="meme-image" class="card-img-top" src="${memeItem.url}" alt="Card image cap"></a>
                     </div>
                     </div>
                 `);
-            }
-        });
-    }
-    
-    function renderGifs(gifArray){
-        gifArray.map((gifItem) => {
-            $(".meme-container").append(`
+      }
+    });
+  }
+
+  function renderGifs(gifArray) {
+    gifArray.map((gifItem) => {
+      $(".meme-container").append(`
                 <div class="meme">
                 <div class="card" style="width: 18rem;">
                 <a href="#" onclick="selectMeme('${gifItem.images.original.url}')"><img id="meme-image" class="card-img-top" src="${gifItem.images.original.url}" alt="Card image cap"></a>
                 </div>
                 </div>
             `);
-        })
-    }
+    });
+  }
 });
 
-function selectMeme(itemURL){
-    $(".meme-container").replaceWith(`
-        <div class="box">
-        <img src="${itemURL}">
-        <input id="top-text" type="text" placeholder="Top Text" />
-        <input id="bottom-text" type="text" placeholder="Bottom Text" />
-        <button class="text-submit">Apply</button>
-        </div>
+function selectMeme(itemURL) {
+  $(".meme-container").replaceWith(`
+      <div class="box">
+          <div>
+            <img src="${itemURL}">
+          </div>
+            <div>
+              <input id="top-text" type="text" placeholder="Top Text" />
+              <input id="bottom-text" type="text" placeholder="Bottom Text" />
+              <button class="text-submit">Apply</button>
+            </div>
+      </div>
     `);
 }
